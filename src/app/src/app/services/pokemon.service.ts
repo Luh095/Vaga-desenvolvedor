207f50f709506a88c485243e8cb86156ba18448a
Luana Roza <91756435+Luh095@users.noreply.github.com>
2025-06-16 11:16:05 -0300

    Create pokemon.service.ts

---
+++ src/app/src/app/services/pokemon.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  private baseUrl = 'https://pokeapi.co/api/v2';
+
+  constructor(private http: HttpClient) {}
+
+  getPokemons(offset: number = 0, limit: number = 20): Observable<any> {
+    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=${limit}`);
+  }
+
+  getPokemonByName(name: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/pokemon/${name}`);
+  }
+}
